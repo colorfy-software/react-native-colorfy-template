0f8ee90d7d643bce839a44ff1442bb62bb4ecfbb
Charles Mangwa <charlesmangwa@gmail.com>
2020-01-13 09:45:58 +0100

    Minor naming changes ✍

--- template/index.js
+++ template/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native'
 import App from './App'
 
-const APP_NAME = 'appStarter'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(APP_NAME, () => App)
+AppRegistry.registerComponent(appName, () => App)
