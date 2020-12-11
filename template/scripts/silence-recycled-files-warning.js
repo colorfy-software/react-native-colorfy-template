49348aacffff1d5595505daa5811aef59bc42178
Kristjan Vool <kristjan@colorfy.me>
2020-12-11 14:38:43 +0100

    fix: wrong paths

--- template/scripts/silence-recycled-files-warning.js
+++ template/scripts/silence-recycled-files-warning.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
+import { readFileSync, writeFileSync } from 'fs'
 
 const codeToObscure = /cycle.push\(cycle\[0\]\);(\s.*){5}/gim
 const problemFilePath = './node_modules/metro/src/lib/polyfills/require.js'
-const problemFileContent = fs.readFileSync(problemFilePath, 'utf8')
-fs.writeFileSync(
+const problemFileContent = readFileSync(problemFilePath, 'utf8')
+writeFileSync(
   problemFilePath,
   problemFileContent.replace(
     codeToObscure,
--- template/src/store/stores.ts
+++ template/src/store/stores.ts
@@ -1,16 +1,12 @@
 import create, { StateCreator, UseStore } from 'zustand'
 import produce from 'immer'
 
-import persist from '../middlewares/persist-middleware'
-import logger from '../middlewares/logger-middleware'
+import persist from './middlewares/persist-middleware'
+import logger from './middlewares/logger-middleware'
 
-import userStore from './user-store'
+import userStore from './stores/user-store'
 
-import {
-  StoreType,
-  StoresDataType,
-  StoresNameType,
-} from '../../types/store-types'
+import { StoreType, StoresDataType, StoresNameType } from '../types/store-types'
 
 // NOTE: Toggle store logging here (done based on Remote JS Debugging)
 window.enableStoreLogging = global.__REMOTEDEV__
