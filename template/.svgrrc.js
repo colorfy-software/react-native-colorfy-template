046597704ca93124bd3f7053a4816af581c2a911
Charles Mangwa <charlesmangwa@gmail.com>
2020-12-15 17:31:18 +0100

    Update comment 📰

--- template/.svgrrc.js
+++ template/.svgrrc.js
@@ -1,7 +1,7 @@
 /**
- * NOTE: Add colors present in the SVG files here.
- * Ideally, SVGs should not have to mention any color.
- * All SVGs should only use one specific color for fill, another for stroke, etc if needed.
+ * NOTE: Add colors present in the SVG icons that you'd like to update from JavaScript here.
+ * Keep in mind that if the SVG doesn't mention any colors, `props.fill` will be used as default.
+ * If need be, any SVG should only use one specific color for fill, another for stroke, etc if needed.
  */
 
 // module.exports = {
